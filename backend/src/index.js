0480645cbf46fac9074b31efe2869c331fb5942e
Marcelo <basescolgate01@gmail.com>
2026-05-16 00:33:44 -0400

    feat: agregar ruta /api/bajas y tab registros de baja

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ app.use('/api/asignaciones', require('./routes/asignaciones'));
 app.use('/api/funcionarios', require('./routes/funcionarios'));
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/tipos', require('./routes/tipos'));
-app.use('/api/areas', require('./routes/areas'));
+app.use('/api/bajas', require('./routes/bajas'));
 
 app.use(express.static(path.join(__dirname, '../../frontend/public')));
 app.get('*', (req, res) => {
